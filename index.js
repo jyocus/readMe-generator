cce60a184fdf329554f46c0720ebeaec9d6cf9c5
Justin Yocus <justin.o.yocus@gmail.com>
2020-10-15 17:46:36 -0500

    imported fs and inquirer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-//importing our generateMarkdown.js
+//importing our generateMarkdown.js, fs, and inquirer
 const generateMarkdown = require('.utils/generateMarkdown');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // array of questions for user
 const questions = [
